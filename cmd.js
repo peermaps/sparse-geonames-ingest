771f3ab69c6e7332f11aca782d30518ec80e25bb
substack <substack@bits.coop>
2022-06-20 00:33:50 -1000

    ingest command

--- cmd.js
+++ cmd.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 var fs = require('fs')
-var path = require('path')
-var pump = require('stream').pipeline
-var Transform = require('stream').Transform
-var split = require('split2')
-var varint = require('varint')
-var uniq = require('uniq')
+var { Transform, pipeline } = require('stream')
 var minimist = require('minimist')
-var bl = Buffer.byteLength
 
 var argv = minimist(process.argv.slice(2), {
   alias: { o: 'outdir' }
@@ -16,28 +10,46 @@ fs.mkdirSync(argv.outdir, { recursive: true })
 
 if (argv._[0] === 'load') {
   var ingest = require('./')({ outdir: argv.outdir })
-  pump(process.stdin, split(), new Transform({
+  load(ingest, onerror)
+} else if (argv._[0] === 'sort') {
+  var ingest = require('./')({ outdir: argv.outdir })
+  ingest.sort(onerror)
+} else if (argv._[0] === 'build') {
+  var ingest = require('./')({ outdir: argv.outdir })
+  build(ingest, onerror)
+} else if (argv._[0] === 'ingest') { // everything
+  var ingest = require('./')({ outdir: argv.outdir })
+  load(ingest, (err) => {
+    if (err) return onerror(err)
+    ingest.sort(function (err) {
+      if (err) return onerror(err)
+      build(ingest, onerror)
+    })
+  })
+}
+
+function load(ingest, cb) {
+  var split = require('split2')
+  pipeline(process.stdin, split(), new Transform({
     transform: function (line, enc, next) {
       ingest.write(line, next)
     },
     flush: function (next) {
       ingest.flush(next)
     }
-  }), onerror)
-} else if (argv._[0] === 'sort') {
-  var ingest = require('./')({ outdir: argv.outdir })
-  ingest.sort(function (err) {
-    console.log(err)
-  })
-} else if (argv._[0] === 'build') {
-  var ingest = require('./')({ outdir: argv.outdir })
+  }), cb)
+}
+
+function build(ingest, cb) {
   var opts = {}
   if (argv.size) opts.size = argv.size
   if (typeof opts.size === 'string') opts.size = Number(opts.size.replace(/_/g,''))
-  ingest.build(opts, function (err) {
-    console.log(err)
-  })
-} else if (argv._[0] === 'ingest') { // everything
+  ingest.build(opts, cb)
 }
 
-function onerror(err) { if (err) console.error(err) }
+function onerror(err) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
